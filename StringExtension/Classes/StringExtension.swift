d0311bb756c38987eaee9551c919ef7b6c21f627
Matteo Crippa <matteo@boostco.de>
2017-02-19 10:16:24 +0100

    html + string

--- StringExtension/Classes/StringExtension.swift
+++ StringExtension/Classes/StringExtension.swift
@@ -64,4 +64,20 @@ public extension String {
         }
         return monogram.uppercased()
     }
+    
+    
+    // MARKL - Html 2 string
+    var html2AttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return nil }
+        do {
+            return try NSAttributedString(data: data, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+            return  nil
+        }
+    }
+    var html2String: String {
+        return html2AttributedString?.string ?? ""
+    }
+
 }
